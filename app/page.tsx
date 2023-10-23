c693c44ec482ebf41032f9938f4c46aee8affb7d
spontaneousflamingo <bertoncelj.mark55@gmail.com>
2023-10-24 00:00:52 +0200

    adds particle background, restructures project

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,22 @@
 "use client";
-import { useEffect, useMemo } from "react";
-import SignSelect from "./components/SignSelect";
 import { useState } from "react";
-import DailyPredictionText from "./components/DailyPredictionText";
-import { StarValueType } from "./interfaces/globalInterfaces";
-import {
-  bumpNumberOfTimesUsed,
-  getLocalStorageSigns,
-} from "./helpers/horoscopeLocalStorageHelper";
+import DailyHoroscope from "./pages/DailyHoroscope";
+import ParticlesBackground from "./components/ParticlesBackground";
+import { CircularProgress } from "@nextui-org/react";
 
 export default function Home() {
-  const [mostVisitedSignKey, setMostVisitedSignKey] = useState<string | null>(
-    null
-  );
-
-  const [sign, setSign] = useState<StarValueType | null>(
-    mostVisitedSignKey as StarValueType
-  );
-
-  useEffect(() => {
-    const localStorageSigns = getLocalStorageSigns();
-
-    const signKeyWithMostVisits =
-      localStorageSigns?.reduce(
-        (
-          maxObject: { numberOfTimesUsed: number; key: string },
-          currentObject: { numberOfTimesUsed: number; key: string }
-        ) => {
-          if (currentObject.numberOfTimesUsed > maxObject.numberOfTimesUsed) {
-            return currentObject;
-          }
-          return maxObject;
-        },
-        localStorageSigns[0]
-      ) || null;
-
-    setMostVisitedSignKey(signKeyWithMostVisits?.key || null);
-  });
-
-  useEffect(() => {
-    if (sign) {
-      bumpNumberOfTimesUsed(sign);
-    }
-  }, [sign]);
+  const [pageLoading, setPageLoading] = useState(true);
 
   return (
-    <main className="flex min-h-screen flex-col items-center md:p-24 p-3">
-      <div className="w-full max-w-4xl">
-        <div className="my-5 w-full">
-          <SignSelect sign={sign} setSign={setSign} />
+    <>
+      <ParticlesBackground setPageLoading={setPageLoading} />
+      {pageLoading ? (
+        <div className="w-screen h-screen flex items-center justify-center bg-black z-[9999999]">
+          <CircularProgress size="lg" />
         </div>
-        <div className="my-5 w-full flex items-center justify-center">
-          <DailyPredictionText sign={sign} key={sign} />
-        </div>
-      </div>
-    </main>
+      ) : (
+        <DailyHoroscope />
+      )}
+    </>
   );
 }
